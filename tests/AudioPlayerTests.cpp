5c039d25ea1acd1202e421d2c2b5428206bfffd6
Mathew Wright <m4_wright@hotmail.com>
2026-03-30 10:59:15 -0400

    More complete tests for audio player including play / pause / toggle / song done

--- tests/AudioPlayerTests.cpp
+++ tests/AudioPlayerTests.cpp
@@ -6,20 +6,64 @@
 #include <future>
 #include <chrono>
 
-
-TEST(AudioPlayer, PlaySong) {
-	AudioPlayer player;
-
-	std::promise<bool> callbackCalledPromise;
+struct PlayWithWait {
+	std::promise<bool> callbackCalledPromise{};
 	std::future<bool> callbackCalledFuture = callbackCalledPromise.get_future();
 
-	// A fake path that isn't used in the mock miniaudio implementation
-	player.play_sound("/tmp/a.mp3", [&] {
-		callbackCalledPromise.set_value(true);
-	});
+	PlayWithWait(AudioPlayer& player, const std::string& path = "/tmp/a.mp3") {
+		player.play_sound(path, [&] {
+			callbackCalledPromise.set_value(true);
+		});
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
 
-	std::future_status status = callbackCalledFuture.wait_for(std::chrono::seconds(1));
+	bool wait() {
+		std::future_status status = callbackCalledFuture.wait_for(std::chrono::seconds(1));
+		return status == std::future_status::ready && callbackCalledFuture.get();
+	}
+};
 
-	EXPECT_EQ(std::future_status::ready, status);
-	EXPECT_EQ(true, callbackCalledFuture.get());
+
+TEST(AudioPlayer, PlaySong_Complete) {
+	AudioPlayer player;
+
+	PlayWithWait waiter{ player };
+	EXPECT_TRUE(player.isPlaying());
+	bool soundCompleted = waiter.wait();
+
+	EXPECT_TRUE(soundCompleted);
+	EXPECT_FALSE(player.isPlaying());
+}
+
+TEST(AudioPlayer, PlayPauseSongComplete) {
+	AudioPlayer player;
+
+	PlayWithWait waiter{ player };
+	EXPECT_TRUE(player.isPlaying());
+
+	player.pause(); 
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	EXPECT_FALSE(player.isPlaying());
+
+	player.resume();
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	EXPECT_TRUE(player.isPlaying());
+
+	bool soundCompleted = waiter.wait();
+	EXPECT_FALSE(player.isPlaying());
+}
+
+TEST(AudioPlayer, Toggle) {
+	AudioPlayer player;
+
+	PlayWithWait waiter{ player };
+	EXPECT_TRUE(player.isPlaying());
+
+	player.toggle();
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	EXPECT_FALSE(player.isPlaying());
+
+	player.toggle();
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	EXPECT_TRUE(player.isPlaying());
 }
--- tests/miniaudio_mocks.h
+++ tests/miniaudio_mocks.h
@@ -2,6 +2,8 @@
 
 #include <thread>
 #include <chrono>
+#include <vector>
+#include <mutex>
 
 enum ma_result {
 	MA_SUCCESS
@@ -17,23 +19,76 @@ ma_result ma_engine_init(void*, ma_engine* engine) {
 void ma_engine_uninit(ma_engine* engine) {}
 
 
+enum class SoundEvent {
+	INIT,
+	UNINIT,
+	START,
+	STOP,
+	TOGGLE
+};
 
+struct ma_sound {
+	std::mutex mtx;
+	std::vector<SoundEvent> events;
+};
 
-struct ma_sound {};
 ma_result ma_sound_init_from_file(ma_engine* engine, const char* path, int, void*, void*, ma_sound* sound) {
+	std::lock_guard<std::mutex> lock(sound->mtx);
+	sound->events.push_back(SoundEvent::INIT);
 	return MA_SUCCESS;
 }
-void ma_sound_uninit(ma_sound* sound) {}
-void ma_sound_start(ma_sound* sound) {}
-void ma_sound_stop(ma_sound* sound) {}
+void ma_sound_uninit(ma_sound* sound) {
+	std::lock_guard<std::mutex> lock(sound->mtx);
+	sound->events.push_back(SoundEvent::UNINIT);
+}
+void ma_sound_start(ma_sound* sound) {
+	std::lock_guard<std::mutex> lock(sound->mtx);
+	sound->events.push_back(SoundEvent::START);
+}
+void ma_sound_stop(ma_sound* sound) {
+	std::lock_guard<std::mutex> lock(sound->mtx);
+	sound->events.push_back(SoundEvent::STOP);
+}
+
+// Returns true if a valid sequence of events has occurred leading to playing
+// That means the sound is started and play has been called more recently than pause
 bool ma_sound_is_playing(const ma_sound* sound) {
-	return true;
+	std::lock_guard<std::mutex> lock(const_cast<ma_sound*>(sound)->mtx);
+
+	bool isInitialized = false;
+	bool isPlaying = false;
+
+	for (const SoundEvent& event : sound->events) {
+		switch (event) {
+		case SoundEvent::INIT:
+			isInitialized = true;
+			break;
+		case SoundEvent::UNINIT:
+			isInitialized = false;
+			break;
+		case SoundEvent::START:
+			isPlaying = true;
+			break;
+		case SoundEvent::STOP:
+			isPlaying = false;
+			break;
+		case SoundEvent::TOGGLE:
+			isPlaying = !isPlaying;
+			break;
+		}
+	}
+
+	return isInitialized && isPlaying;
 }
 
 typedef void (*ma_sound_end_proc)(void* pUserData, ma_sound* pSound);
 ma_result ma_sound_set_end_callback(ma_sound* sound, ma_sound_end_proc callback, void* userData) {
 	std::thread([=] {
-		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		{
+			std::lock_guard<std::mutex> lock(sound->mtx);
+			sound->events.push_back(SoundEvent::STOP);
+		}
 		callback(userData, sound);
 	}).detach();
 
